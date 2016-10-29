6aa30eed91988482641e51f21bd457df93c146a7
Alan Guo <aguo5520@gmail.com>
2016-10-29 12:42:29 -0400

    Skeleton infrastructure for views and js.

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,23 @@
+/**
+ * Created by alanguo on 10/29/16.
+ */
+var spotifyCollab = angular.module('spotifyCollabApp', ['ngRoute']);
+spotifyCollab.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '../views/home.html',
+            controller: 'homeCtrl'
+        })
+        .when('/login', {
+            templateUrl: '../views/login.html',
+            controller: 'loginCtrl'
+        })
+        .when('/logout', {
+            templateUrl: '../views/logout.html',
+            controller: 'logoutCtrl'
+        })
+        .otherwise({
+            templateUrl: '../views/home.html',
+            controller: 'homeCtrl'
+        })
+});
