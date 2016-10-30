dc45e4fdefab3faf831ffd2969dd0399a02686fd
Jiawei Zhang <jiaweizhang95@gmail.com>
2016-10-29 22:10:16 -0400

    Modified database schema

--- sql/setup.sql
+++ sql/setup.sql
@@ -4,64 +4,62 @@ CREATE SCHEMA public;
 
 /* Users table */
 CREATE TABLE IF NOT EXISTS users (
-  spotify_userid     VARCHAR(50)  NOT NULL,
-  email              VARCHAR(255) NOT NULL,
+  spotify_id         VARCHAR(50)   NOT NULL,
+  email              VARCHAR(255)  NOT NULL,
   authorization_code VARCHAR(4000) NOT NULL,
   refresh_token      VARCHAR(4000) NOT NULL,
   access_token       VARCHAR(4000),
   expiration         TIMESTAMP,
-  CONSTRAINT PK_users PRIMARY KEY (spotify_userid)
+  CONSTRAINT PK_users PRIMARY KEY (spotify_id)
 );
 
-
-/* PR Playlists table */
-CREATE TABLE IF NOT EXISTS master_playlists (
-  playlist_id VARCHAR(50) NOT NULL, --this is the master playlist id on spotify (ie LIT AF)
+/* Playlists table */
+CREATE TABLE IF NOT EXISTS playlists (
+  playlist_id   VARCHAR(50)  NOT NULL, --this is the master playlist id on spotify (ie LIT AF)
   playlist_name VARCHAR(255) NOT NULL,
-  owner_id    VARCHAR(50) NOT NULL,
-  threshold   INTEGER     NOT NULL
+  owner_id      VARCHAR(50)  NOT NULL,
+  threshold     INTEGER      NOT NULL
     CHECK (threshold >= 0 AND threshold <= 100  ), --this is the threshold for each song
-  CONSTRAINT PK_master_playlists PRIMARY KEY (playlist_id),
-  CONSTRAINT FK_master_playlists_owner_id FOREIGN KEY (owner_id) REFERENCES users (spotify_userid)
+  CONSTRAINT PK_playlists PRIMARY KEY (playlist_id),
+  CONSTRAINT FK_playlists_owner_id FOREIGN KEY (owner_id) REFERENCES users (spotify_id)
 );
 
-CREATE TABLE IF NOT EXISTS master_songs (
-  id          SERIAL      NOT NULL,
+/* Playlists PR table */
+CREATE TABLE IF NOT EXISTS playlists_pr (
+  playlist_id        VARCHAR(50)  NOT NULL,
+  playlist_name      VARCHAR(255) NOT NULL,
+  owner_id           VARCHAR(50)  NOT NULL,
+  parent_playlist_id VARCHAR(50)  NOT NULL,
+  CONSTRAINT PK_playlists_pr PRIMARY KEY (playlist_id),
+  CONSTRAINT FK_playlists_pr_owner_id FOREIGN KEY (owner_id) REFERENCES users (spotify_id),
+  CONSTRAINT FK_playlists_pr_parent_playlist_id FOREIGN KEY (parent_playlist_id) REFERENCES playlists (playlist_id)
+);
+
+CREATE TABLE IF NOT EXISTS requests (
+  request_id  SERIAL      NOT NULL,
   playlist_id VARCHAR(50) NOT NULL,
-  contributor VARCHAR(50) NOT NULL,
+  spotify_id  VARCHAR(50) NOT NULL,
   song_id     VARCHAR(50) NOT NULL,
-  CONSTRAINT PK_master_songs PRIMARY KEY (id),
-  CONSTRAINT FK_master_songs_playlist_id FOREIGN KEY (playlist_id) REFERENCES master_playlists (playlist_id),
-  CONSTRAINT FK_master_songs_contributor FOREIGN KEY (contributor) REFERENCES users (spotify_userid)
+  CONSTRAINT PK_requests PRIMARY KEY (request_id),
+  CONSTRAINT FK_requests_playlist_id FOREIGN KEY (playlist_id) REFERENCES playlists_pr (playlist_id),
+  CONSTRAINT FK_requests_spotify_id FOREIGN KEY (spotify_id) REFERENCES users (spotify_id)
 );
 
 /* Master contributors table */
-CREATE TABLE IF NOT EXISTS master_contributors (
-  playlist_id VARCHAR(50) NOT NULL,
-  collab_id VARCHAR(50) NOT NULL,
-  CONSTRAINT FK_master_contributors_playlist_id FOREIGN KEY (playlist_id) REFERENCES master_playlists(playlist_id),
-  CONSTRAINT FK_master_contributors_collab_id FOREIGN KEY (collab_id) REFERENCES users(spotify_userid)
+CREATE TABLE IF NOT EXISTS contributors (
+  playlist_id    VARCHAR(50) NOT NULL,
+  playlist_pr_id VARCHAR(50) NOT NULL,
+  spotify_id     VARCHAR(50) NOT NULL,
+  CONSTRAINT FK_contributors_playlist_id FOREIGN KEY (playlist_id) REFERENCES playlists (playlist_id),
+  CONSTRAINT FK_contributors_playlist_pr_id FOREIGN KEY (playlist_pr_id) REFERENCES playlists_pr (playlist_id),
+  CONSTRAINT FK_contributors_spotify_id FOREIGN KEY (spotify_id) REFERENCES users (spotify_id)
 );
-
-/* Individual PR Playlists table */
-CREATE TABLE IF NOT EXISTS individual_playlists (
-  spotify_playlist_id VARCHAR(50) NOT NULL,
-  owner_id            VARCHAR(50) NOT NULL,
-  master_id           VARCHAR(50) NOT NULL,
-  CONSTRAINT PK_individual_playlists PRIMARY KEY (spotify_playlist_id),
-  CONSTRAINT FK_individual_playlists_master_id FOREIGN KEY (master_id) REFERENCES master_playlists (playlist_id)
-);
-
 
 /* Vote table */
 CREATE TABLE IF NOT EXISTS vote_table (
-  song_id  INTEGER     NOT NULL,
-  voted_by VARCHAR(50) NOT NULL,
-  vote     BOOLEAN     NOT NULL,
-  CONSTRAINT FK_vote_table_song_id FOREIGN KEY (song_id) REFERENCES master_songs (id),
-  CONSTRAINT FK_vote_table_voted_by FOREIGN KEY (voted_by) REFERENCES users (spotify_userid)
+  request_id VARCHAR(50) NOT NULL,
+  spotify_id VARCHAR(50) NOT NULL,
+  vote       BOOLEAN     NOT NULL,
+  CONSTRAINT FK_vote_request_id FOREIGN KEY (request_id) REFERENCES requests (request_id),
+  CONSTRAINT FK_vote_spotify_id FOREIGN KEY (spotify_id) REFERENCES users (spotify_id)
 );
-
-
-
-/* Upvote/downvote table */
