6e699331c34dc5011639806cb950a873bae09722
Jiawei Zhang <jiaweizhang95@gmail.com>
2016-10-30 05:10:11 -0400

    added int

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -109,5 +109,7 @@ spotifyCollab.controller('playlistController', function ($scope, $http) {
 
     $scope.init = function () {
         $scope.getPlaylist();
-    }
+    };
+
+    $scope.init();
 });
