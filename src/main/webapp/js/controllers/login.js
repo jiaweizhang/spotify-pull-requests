128a876dd2ead5893250286294e524fc0bfb1ab0
Jiawei Zhang <jiaweizhang95@gmail.com>
2016-10-30 01:47:14 -0400

    Removed links

--- src/main/webapp/js/controllers/login.js
+++ src/main/webapp/js/controllers/login.js
@@ -2,7 +2,7 @@
  * Created by alanguo on 10/29/16.
  */
 spotifyCollab
-    .controller('loginCtrl', [ '$scope', 'Auth', function ($scope, Auth, $window) {
+    .controller('loginCtrl', [ '$scope', function ($scope, $window) {
         var link = "https://accounts.spotify.com/authorize/?client_id=7f5795cf20ba45b08ca27a4a8f92cf23&response_type=code&redirect_uri=http%3A%2F%2Feasywebapi.com%3A8080%2Fredirect&scope=playlist-read-private%20playlist-modify-public%20playlist-modify-private&state=spr"
 
         //submit
