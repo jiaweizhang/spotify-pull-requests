6ba5c5b4c044da2951ec2a142aaecbe09aca14ee
Jiawei Zhang <jiaweizhang95@gmail.com>
2016-10-30 01:43:57 -0400

    Fixed data router

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -26,3 +26,40 @@ spotifyCollab.config(function ($routeProvider) {
             controller: 'homeCtrl'
         })
 });
+
+spotifyCollab.run(function ($rootScope) {
+    $rootScope.checkCookies = function () {
+        var preval = localStorage.getItem('auth');
+        if (preval == null) {
+            $rootScope.loggedIn = false;
+            console.log("auth token is not found");
+            return false;
+        } else {
+            $rootScope.loggedIn = true;
+            console.log("auth token is found: " + preval);
+            return true;
+        }
+    };
+
+});
+
+spotifyCollab.run(function ($rootScope, $location) {
+
+    // register listener to watch route changes
+    $rootScope.$on("$routeChangeStart", function (event, next, current) {
+        console.log("change route");
+        if ($rootScope.checkCookies() == false) {
+            console.log($location.path());
+            /*
+            if ($location.path() == '/lumitrivia') {
+                console.log("Switching to login");
+                $location.path("/login");
+            }
+            */
+            $location.path("/");
+            //if ($location.path() == '/login') {
+            //    $location.path("/lumitrivia");
+            //}
+        }
+    });
+});
