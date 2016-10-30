b4ba2f5bdcd307127f39812256a959c820588a3a
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-10-30 00:20:13 -0400

    updated the contributor accessor to have the additional get request

--- src/main/java/data/data_accessors/ContributorAccessor.java
+++ src/main/java/data/data_accessors/ContributorAccessor.java
@@ -1,11 +1,13 @@
 package data.data_accessors;
 
 import data.Contributor;
+import data.PlaylistPr;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static db.tables.Contributors.CONTRIBUTORS;
+import static db.tables.PlaylistsPr.PLAYLISTS_PR;
 
 public class ContributorAccessor extends Accessor {
 
@@ -25,5 +27,16 @@ public class ContributorAccessor extends Accessor {
                         record.getValue(CONTRIBUTORS.PLAYLIST_PR_ID),
                         record.getValue(CONTRIBUTORS.SPOTIFY_ID)
                 )).collect(Collectors.toList());
+    }
+
+    public List<PlaylistPr> getPlaylistPrBasedOnContributor(String contributorId) {
+        List<String> playlistPrIds = myQuery.select(CONTRIBUTORS.PLAYLIST_ID).from(CONTRIBUTORS)
+                .where(CONTRIBUTORS.SPOTIFY_ID.eq(contributorId)).fetch().map(record -> record.getValue(CONTRIBUTORS.PLAYLIST_ID));
+        return myQuery.select().from(PLAYLISTS_PR).where(PLAYLISTS_PR.PLAYLIST_ID.in(playlistPrIds)).fetch().stream().map(record -> new PlaylistPr(
+                record.getValue(PLAYLISTS_PR.PLAYLIST_ID),
+                record.getValue(PLAYLISTS_PR.PLAYLIST_NAME),
+                record.getValue(PLAYLISTS_PR.OWNER_ID),
+                record.getValue(PLAYLISTS_PR.PLAYLIST_ID)
+        )).collect(Collectors.toList());
     }
 }
